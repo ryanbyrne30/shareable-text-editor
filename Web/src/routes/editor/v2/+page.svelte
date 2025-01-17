7c36adf2adb005c1a911b66dcb672a549017331c
ryanbyrne30 <ryanjbyrne30@gmail.com>
2025-01-17 14:29:48 -0700

    feat: sync scrolling with preview

--- Web/src/routes/editor/v2/+page.svelte
+++ Web/src/routes/editor/v2/+page.svelte
@@ -1,30 +1,68 @@
 <script lang="ts">
 	import { marked } from 'marked';
+	import { onMount } from 'svelte';
 	import { twMerge } from 'tailwind-merge';
 
+	let textarea: HTMLTextAreaElement | null = null;
 	let content: HTMLDivElement | null = null;
 	let text: string = '';
 	let showPreview = false;
 	let orientation: 'horizontal' | 'vertical' = 'vertical';
+	let disableContentScroll = false;
+	let disableTextScroll = false;
+	let disableContentScrollTimeout = 0;
+	let disableTextScrollTimeout = 0;
+	let scrollLockTimeoutMs = 500;
 
 	function render() {
 		if (content === null) return;
 		content.innerHTML = marked.parse(text) as string;
 	}
 
-	const contentConfig = {
-		horizontal: 'h-1/2 w-full',
-		vertical: 'w-1/2 h-full',
-		default: 'hidden'
-	};
+	function syncScroll(fromEl: HTMLElement, toEl: HTMLElement) {
+		const yprop = getScrollYProportion(fromEl);
+		const xprop = getScrollXProportion(fromEl);
+		const { height, width } = toEl.getBoundingClientRect();
+		toEl.scrollTop = yprop * (toEl.scrollHeight - height);
+		toEl.scrollLeft = xprop * (toEl.scrollWidth - width);
+	}
 
-	function getContentConfig(showPreview: boolean, orientation: string) {
-		if (!showPreview) return contentConfig.default;
-		if (orientation === 'horizontal') return contentConfig.horizontal;
-		return contentConfig.vertical;
+	function getScrollYProportion(el: HTMLElement) {
+		const scroll = el.scrollTop;
+		const height = el.scrollHeight - el.getBoundingClientRect().height;
+		return scroll / height;
+	}
+
+	function getScrollXProportion(el: HTMLElement) {
+		const scroll = el.scrollLeft;
+		const width = el.scrollWidth - el.getBoundingClientRect().width;
+		return scroll / width;
 	}
 
 	$: text !== undefined && render();
+
+	onMount(() => {
+		if (!textarea || !content) return;
+		textarea.addEventListener('scroll', function () {
+			if (!content || !textarea || disableTextScroll) return;
+			clearTimeout(disableContentScrollTimeout);
+			disableContentScroll = true;
+			syncScroll(textarea, content);
+			disableContentScrollTimeout = setTimeout(() => {
+				disableContentScroll = false;
+			}, scrollLockTimeoutMs);
+		});
+
+		content.addEventListener('scroll', function () {
+			if (!content || !textarea || disableContentScroll) return;
+			clearTimeout(disableTextScrollTimeout);
+			disableTextScroll = true;
+			syncScroll(content, textarea);
+			disableTextScrollTimeout = setTimeout(() => {
+				disableTextScroll = false;
+			}, scrollLockTimeoutMs);
+		});
+	});
 </script>
 
 <div class="flex flex-row gap-4 p-4">
@@ -44,6 +82,7 @@
 	)}
 >
 	<textarea
+		bind:this={textarea}
 		bind:value={text}
 		class={twMerge('h-full w-full resize-none border border-black p-4 font-mono')}
 	></textarea>
